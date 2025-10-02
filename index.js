ecf193de19eaed4bc497991919d283c6fe6ebcf2
VictoriaWiley <victoriawiley1992@gmail.com>
2025-10-02 11:38:12 -0500

    added javascript listeners

---
+++ index.js
@@ -0,0 +1,10 @@
+/*event listener*/
+document.getElementById(button).addEventListener('click', function() {
+    const name = document.getElementById('Name').ariaValueText;
+    const age = document.getElementById('age').ariaValueText;
+    const birthmonth = document.getElementById('Birthmonth')(.ariaValueMin-.ariaValueMax);
+}
+/* */
+/* */
+/* */
+/* */
