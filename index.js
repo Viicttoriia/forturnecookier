7360d17cd11ac7313da804f7c513dbadb0df8a6b
VictoriaWiley <victoriawiley1992@gmail.com>
2025-10-02 16:33:04 -0500

    rought draft for js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
 document.getElementById(button).addEventListener('click', function() {
     const name = document.getElementById('Name').ariaValueText;
     const age = document.getElementById('age').ariaValueText;
-    const birthmonth = document.getElementById('Birthmonth'),(.ariaValueMin-.ariaValueMax);
+    const Birthmonth = document.getElementById('Birthmonth'),(.ariaValueMin-.ariaValueMax);
     const outputDiv = document.getElementById('fort-output');
 }
 /* var for output */
@@ -17,6 +17,9 @@ if (!Name || !Age || !Birthmonth) {
     outputDiv.textContent = "Please make slection";
     return;
 }
+
+/* */
+
 /* fortune consts */
 const fortunes = [
     "A goblin's shiny bauble is another's misplaced heirloom. Check your pockets.",
@@ -25,5 +28,14 @@ const fortunes = [
     "Today is an excellent day for chaotic, poorly thought-out plans.",
     "Not all who wander are lost, but many who follow goblins are.",
 ];
-/* */
-/* */
+/* randomizer */
+const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+
+/* function */
+
+if (age > 7) { introduction = 'Hello, Spellcaster ${Name}! Someone so young of age! Your Fate is not Bound! But here is Todays fortunes.'} else if (age >= 7 && age <20) { introduction = 'Hello Warrior ${Name}! So brave of you to reveal your fate.'} else { introduction = 'GrandMaster ${Name}, Your Fate for the day is Fortold';
+}
+/* output */
+
+outputDiv.textContent = introduction + randomFortune;
+});
